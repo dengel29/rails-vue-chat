0c67b4b51d4eba34e96146b71e90fff2e41ac6fb
Dan Engel <dengel29@gmail.com>
2021-04-08 15:50:10 +0800

    test connection with test user in fixtures/users.yml works

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "42"
-  # end
+  def test_connection_success_when_cookie_is_set_correctly
+    user = users(:dan)
+    cookies.signed["current_user_id"] = user.id 
+    
+    # Simulate the connection
+    connect
+
+    # Assert if the correct user is set
+    assert_equal user.id, connection.current_user.id
+  end
+
+  def test_connection_rejected_without_cookie_set
+    assert_reject_connection { connect }
+  end
 end
