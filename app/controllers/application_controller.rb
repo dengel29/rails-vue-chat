2aebc34ab2c8b625912e64691bc4669bb7cda560
Dan Engel <dengel29@gmail.com>
2021-04-08 13:37:51 +0800

    user model + sign in with devise, redirects to chat page. main.js vue app configured to allow drop-in vue components

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email, :password, :remember_me)
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    chats_path_url
+  end
+
+  def after_sign_up_path_for(scope)
+    chats_path_url
+  end
+end
