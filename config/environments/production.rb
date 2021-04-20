450ce42d04a594e05b00f87db658850abc34fc5c
Dan Engel <dengel29@gmail.com>
2021-04-20 11:49:50 +0800

    added localhost to allowed connection origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://rails-vue-actioncable.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https:rails-vue-actioncable.herokuapp.com', /http:\/\/rails-vue-actioncable.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https:rails-vue-actioncable.herokuapp.com', /http:\/\/rails-vue-actioncable.herokuapp.com.*/, 'http://localhost:3000' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
