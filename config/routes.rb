ed487659725e51f21cff4c5b355617a0a332a430
Dan Engel <dengel29@gmail.com>
2021-04-22 12:32:22 +0800

    root route to /chats

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  root "chats#index"
 
   get "/chats" => "chats#index", as: :chats_path
 
